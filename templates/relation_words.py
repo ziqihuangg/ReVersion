4b24980106ecb0b864c64870f5061f0b2ccf31ea
ziqihuangg <connie.huangziqi@gmail.com>
2023-08-21 17:16:49 +0800

    add training code

---
+++ templates/relation_words.py
@@ -0,0 +1,10 @@
+relation_words = [
+    "aboard", "about", "above", "across", "after", "against", "along",
+    "alongside", "amid", "amidst", "among", "amongst", "anti", "around",
+    "astride", "at", "atop", "before", "behind", "below", "beneath", "beside",
+    "between", "beyond", "by", "down", "following", "from", "in", "including",
+    "inside", "into", "near", "of", "off", "on", "onto", "opposite", "out",
+    "outside", "over", "past", "regarding", "round", "through", "throughout",
+    "to", "toward", "towards", "under", "underneath", "up", "upon", "versus",
+    "with", "within"
+]
